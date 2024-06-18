07edf088eefc4eeabdea6cf066732ae1cab45f37
srinivasulu0514 <56268088+srinivasulu0514@users.noreply.github.com>
2024-06-18 13:52:24 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello-World Devops 10741843";
+        return "Hello-World Devops 107418434;
     }
 
 }
